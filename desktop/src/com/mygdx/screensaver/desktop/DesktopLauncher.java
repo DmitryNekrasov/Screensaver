8f29bad954a2da23fbfbdee5dc7efca3535cb464
DmitryNekrasov <scannorone@gmail.com>
2015-05-22 12:24:57 +0600

    + спираль

--- desktop/src/com/mygdx/screensaver/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/screensaver/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygdx.screensaver.Screensaver;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+                config.width = 1440;
+                config.height = 900;
+//                config.fullscreen = true;
 		new LwjglApplication(new Screensaver(), config);
 	}
 }
