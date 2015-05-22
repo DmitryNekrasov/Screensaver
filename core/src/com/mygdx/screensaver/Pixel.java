2ca60a00ad8841558a5e2f491e8dc25aa1c908cf
DmitryNekrasov <scannorone@gmail.com>
2015-05-22 09:43:15 +0600

    + класс Pixel

---
+++ core/src/com/mygdx/screensaver/Pixel.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mygdx.screensaver;
+
+/**
+ *
+ * @author ScanNorOne
+ */
+public class Pixel {
+    
+    public float R, G, B;
+    public int counter;
+    
+    public Pixel() {
+        R = G = B = 0;
+        counter = 0;
+    }
+    
+}
--- core/src/com/mygdx/screensaver/Screensaver.java
+++ core/src/com/mygdx/screensaver/Screensaver.java
@@ -14,11 +14,13 @@ public class Screensaver extends ApplicationAdapter {
 	Texture img;
         ShapeRenderer sr;
         
-        final int XMIN = 0, XMAX = 300;
+        final float XMIN = -1.5f, XMAX = 1.5f;
+        final float YMIN = -1f, YMAX = 1f;
         
         final int n = 20;
         final int countCoefficient = 20;
         final int it = 20;
+        final int xRes = 256, yRes = 256;        
         
         ArrayList<Coefficient> cf;
 	
@@ -47,24 +49,38 @@ public class Screensaver extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-//		Gdx.gl.glClearColor(1, 0, 0, 1);
-//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-//		batch.begin();
-                
                 Gdx.gl.glClearColor(0.01f, 0.01f, 0.01f, 1);
                 Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        
                 sr.setAutoShapeType(true);
-                
                 sr.begin();
                 
-                sr.end();
+                for (int num = 0; num < n; num++) {
+                    float newX = (float) Math.random() * 3 - 1.5f;
+                    float newY = (float) Math.random() * 2 - 1;
+                    for (int step = -20; step < it; step++) {
+                        int i = generateRandom(0, countCoefficient - 1);
+                        float x = cf.get(i).a * newX + cf.get(i).b * newY + cf.get(i).c;
+                        float y = cf.get(i).d * newX + cf.get(i).e * newY + cf.get(i).f;
+                        if (step >= 0) {
+                            float x1 = xRes - Trunc(((XMAX - newX) / (XMAX - XMIN)) * xRes);
+                            float y1 = yRes - Trunc(((YMAX - newY) / (YMAX - YMIN)) * yRes);
+                            if(x1 < xRes && y1 < yRes) {
+                                
+                            }
+                        }
+                    }
+                }
                 
-//		batch.end();
+                sr.end();
 	}
         
         public static Random random = new Random();
         public static int generateRandom(int left, int right) {
             return Math.abs(random.nextInt()) % (right - left + 1) + left;
         }
+        
+        public float Trunc(float a) {
+            return a;
+        }
+        
 }
