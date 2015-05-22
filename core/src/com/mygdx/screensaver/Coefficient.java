7aa83a9f0f623eb3032700ed07a236a8be75daca
DmitryNekrasov <scannorone@gmail.com>
2015-05-22 09:12:30 +0600

    + класс Coefficient

---
+++ core/src/com/mygdx/screensaver/Coefficient.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mygdx.screensaver;
+
+/**
+ *
+ * @author ScanNorOne
+ */
+public class Coefficient {
+    
+    public float a, b, c, d, e, f;
+    public float R, G, B;
+    
+    public Coefficient(float a, float b, float c, float d, float e, float f, float R, float G, float B) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
+        this.R = R;
+        this.G = G;
+        this.B = B;
+    }
+    
+}
--- core/src/com/mygdx/screensaver/Screensaver.java
+++ core/src/com/mygdx/screensaver/Screensaver.java
@@ -5,15 +5,36 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import java.util.ArrayList;
 
 public class Screensaver extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+        
+        final int countCoefficient = 20;
+        ArrayList<Coefficient> cf;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+                cf = new ArrayList<Coefficient>();
+                for (int i = 0; i < countCoefficient; i++) {
+                    float a = (float) Math.random() * 2 - 1;
+                    float b = (float) Math.random() * 2 - 1;
+                    float d = (float) Math.random() * 2 - 1;
+                    float e = (float) Math.random() * 2 - 1;
+                    
+                    float c = (float) Math.random() * 2 + 1;
+                    float f = (float) Math.random() * 2 + 1;
+                    
+                    float R = (float) Math.random();
+                    float G = (float) Math.random();
+                    float B = (float) Math.random();
+                    
+                    cf.add(new Coefficient(a, b, c, d, e, f, R, G, B));
+                    
+                }
 	}
 
 	@Override
